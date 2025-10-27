3ea8910e446bcd142c89dfbed3539ed135896fb3
olliepear <51545134+olliepear@users.noreply.github.com>
2025-10-27 12:17:49 -0600

    Update bitburnerProgram1.js

--- bitburnerProgram1.js
+++ bitburnerProgram1.js
@@ -4,7 +4,7 @@ export async function main(ns) {
   var numberOfNeigbors = neigbor.length;
   for (let i = 0; i < numberOfNeigbors; i++) {
     ns.nuke(neigbor[i]);
-    await ns.wget("https://raw.githubusercontent.com/olliepear/randomProjects/main/bitburnerProgram1.js", neigbor[i], "ultimate.js");
+    await ns.wget("https://raw.githubusercontent.com/olliepear/randomProjects/main/bitburnerProgram1.js", "ultimate.js", neigbor[i]);
     ns.exec(ultimate.js, neigbor[i]);
   }
 
