61b17a7c35596a3422ff8a00f98188cf23b59bc1
olliepear <51545134+olliepear@users.noreply.github.com>
2025-10-27 12:10:30 -0600

    Create bitburnerProgram1.js

---
+++ bitburnerProgram1.js
@@ -0,0 +1,16 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  var neigbor = ns.scan(ns.get.Hostname);
+  var numberOfNeigbors = neigbor.length;
+  for (let i = 0; i < numberOfNeigbors; i++) {
+    ns.nuke(neigbor[i]);
+    await ns.wget("https://raw.githubusercontent.com/olliepear/randomProjects/main/bitburnerProgram1.js", neigbor[1], ultimate.js)
+    ns.exec(ultimate.js, neigbor[i]);
+  }
+
+  while (true) {
+    for (let i = 0; i <numberOfNeigbors; i++) {
+      await ns.hack(neigbor[i]);
+    }
+  }
+}
